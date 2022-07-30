c3de27dba0b2a86293ed9c19dff91ddc2270a5af
Diego Alejandro <diales1981@gmail.com>
2022-07-30 11:20:27 -0500

    Comenzando proyecto utilizando CRUD con Spring Web

---
+++ src/main/java/com/diamones/springboot/app/SpringBootMiPrimerCrudApplication.java
@@ -0,0 +1,13 @@
+package com.diamones.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMiPrimerCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMiPrimerCrudApplication.class, args);
+	}
+
+}
